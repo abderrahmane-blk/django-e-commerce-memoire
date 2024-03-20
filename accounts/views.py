680692c059ef3cf943901fb40e912ec6f41b0512
abderrahmane-blk <dahmanezitouna@gmail.com>
2024-03-20 22:28:49 +0100

    login with email

--- accounts/views.py
+++ accounts/views.py
@@ -35,6 +35,14 @@ def register_vendor(request):
             if password1 and password2 and password1 !=password2:
                 form2 = New_vendor_form(request.POST)
                 return render(request , 'registration/register_vendor.html' ,{'form':form2 ,'error' :'passwords did not match'})
+            if User.objects.filter(username=username):
+                form2 = New_vendor_form(request.POST)
+                return render(request , 'registration/register_vendor.html' ,{'form':form2 ,'error' :'this username is already taken'})
+            
+            if User.objects.filter(email=email):
+                form2 = New_vendor_form(request.POST)
+                return render(request , 'registration/register_vendor.html' ,{'form':form2 ,'error' :'this email is already registerd for another user'})
+            
 
             new_user=User.objects.create(username=username , password=make_password(password1) ,email=email,first_name=first_name ,last_name=last_name )
 
@@ -46,14 +54,12 @@ def register_vendor(request):
             Vendor_info.objects.create(vendor_specific_property=vendor_specific_property , store_deposit_account=store_deposit_account ,user=new_user)
 
             
-            print(new_user)
+            #print(new_user)
 
 
         return redirect('login')
         pass
 
-    
-    
     form =New_vendor_form()
     return render(request,'registration/register_vendor.html' ,{'form':form})
 
